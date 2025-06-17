f2f5cebade9274a600aaca04dd3a59f8572b9ac4
Valiant719 <qiexu719@126.com>
2025-06-18 00:26:11 +0800

    update: to_dict, to_json

--- src/ghostcut_sdk/ghostcut_type/ghostcut_base_model.py
+++ src/ghostcut_sdk/ghostcut_type/ghostcut_base_model.py
@@ -1,5 +1,12 @@
+from typing import Dict
 from pydantic import BaseModel, ConfigDict
 
 
 class GhostcutBaseModel(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
+
+    def to_dict(self) -> Dict:
+        return self.model_dump(exclude_none=True, by_alias=True)
+
+    def to_json(self, indent: int = 4) -> str:
+        return self.model_dump_json(indent=indent, exclude_none=True, by_alias=True)
